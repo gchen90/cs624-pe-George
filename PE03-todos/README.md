e9439f3e4caeb4a52eb66a62ed655988ff7f6646
gchen90 <chengeorge@cityuniversity.edu>
2025-05-05 03:59:48 +0000

    Submission for PE03 - George Chen

--- PE03-todos/README.md
+++ PE03-todos/README.md
@@ -9,7 +9,11 @@ todo object, iterates the variable todoIndex, pushes the todo object into the
 todo array, and sets the state to match the updated array.
 
 # Output
-This app only outputs to the log at the moment. When the inputChange function
-is called, it prints the value to the log allowing us to track what the input.
-When submitTodo function is called, it prints the current state to the log so 
-we can see what items are in the todo array.
+The TodoList is populated with the todos array, and provides options to complete
+the todo item or delete it. If the delete button is pressed, the specific todo is 
+filtered out of todos array and the state is set. If the complete button is pressed, 
+it will flip the completed boolean for that todo and set the state. The TodoList
+also filters itself based on the type (prop). When the "All" TabBarItem is selected,
+the type is set to "All", and all items in todos array are returned. Similarly,
+"Active" will show only todo items that have not been completed, and "Completed"
+will show only todo items that have been completed.
--- PE03-todos/todos/app/(tabs)/App.js
+++ PE03-todos/todos/app/(tabs)/App.js
@@ -3,6 +3,9 @@ import {View, ScrollView, StyleSheet} from 'react-native';
 import Heading from './Heading';
 import Input from './Input';
 import Button from './Button';
+import TodoList from './TodoList';
+import TabBar from './TabBar'
+
 
 let todoIndex = 0
 
@@ -15,6 +18,29 @@ class App extends Component {
             type: 'All',
         };
         this.submitTodo = this.submitTodo.bind(this);
+        this.toggleComplete = this.toggleComplete.bind(this)
+        this.deleteTodo = this.deleteTodo.bind(this)
+        this.setType = this.setType.bind(this)
+    }
+
+    deleteTodo (todoIndex) {
+        let {todos} = this.state
+        todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+        this.setState({ todos })
+    }
+
+    toggleComplete (todoIndex) {
+        let todos = this.state.todos
+        todos.forEach((todo) => {
+            if (todo.todoIndex === todoIndex) {
+                todo.complete = !todo.complete
+            }
+        })
+        this.setState({todos})
+    }
+
+    setType (type) {
+        this.setState({type})
     }
 
     inputChange(inputValue) {
@@ -39,17 +65,22 @@ class App extends Component {
     }
 
     render() {
-        const {inputValue} = this.state;
+        const {inputValue, todos, type} = this.state;
         return (
             <View style={styles.container}>
-                <ScrollView keyboardShouldPersistTaps="always" style={styles.content}>
+                <ScrollView style={styles.content}>
                     <Heading />
                     <Input
                         inputValue={inputValue}
                         inputChange={text => this.inputChange(text)}
                     />
-                    <Button submitTodo={this.submitTodo} />
+                    <TodoList type={type}
+                        toggleComplete={this.toggleComplete}
+                        deleteTodo={this.deleteTodo.bind(this)}
+                        todos={todos} />
+                    <Button submitTodo={() => this.submitTodo()} />
                 </ScrollView>
+                <TabBar type={type} setType={this.setType.bind(this)} />
             </View>
         );
     }        
